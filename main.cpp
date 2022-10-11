84a727302ef5f1900916cfb209dc4d0aaf471402
GeoffNixCodes <97037675+GeoffNixCodes@users.noreply.github.com>
2022-10-10 20:40:40 -0400

    Initial files

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+
+#include "InputMenu.h"
+
+
+using namespace std;
+
+int main() {
+	//MAIN WILL FIRST CALL MENU TO PRINT OUT OPTIONS
+	InputMenu();
+
+	return 0;
+
+}
